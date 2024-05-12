106af0e47982517ef59167cf890bc885d8c8107f
wtf2408 <bogdanevseyv@yandex.ru>
2024-05-12 14:08:39 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,139 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+//  Посмотреть список пользователей
+//	1) Для каждого пользователя хранится следующая информация: 
+//	- фамилия
+//	- имя
+//	- отчество
+//	- номер телефон
+//	- e - mail
+//	- логин
+//	- пароль
+
+//	2) Пароль должен храниться и отображаться в форме ХЭШ-кода. 
+//	3) Фамилия, имя и отчество записаны с большой буквы. 
+//	4) Номер телефона и e-mail должны быть корректны. 
+//	5) При вводе информации необходимо проверять корректность ввода полей с помощью регулярных выражений. ФИО должны меняться в соответствии с правилом 3), если телефон записан корректно, то сохранять его в форме +7-(222)-222-22-22
+//	6) пароль должен не менее 8 символов, содержать как минимум одну заглавную букву, одну строчную, одну цифру, один специальный символ. Проверка осуществляется с помощью регулярных выражений
+//	7) ФИО не должны содержать цифр
+
+//	Добавить пользователя
+//	Удалить пользователя
+//	Изменить пользователя
+//	Сохранить изменения в файл
+//	Отправить сообщение на e-mail пользователя
+//	Отсортировать по выбранному полю
+
+namespace CommonTask
+{
+    class Person
+    {
+        static Person()
+        {
+
+            sha256 = SHA256.Create();
+            phoneRegex = new Regex(@"^(\+7|8)\d{10}$");
+            passwordRegex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$");
+            emailRegex = new Regex(@"^\w+@\w+\.[a-z]{2,4}$");
+            nameRegex = new Regex(@"^[А-Я A-Z][a-z а-я]* [А-Я A-Z][a-z а-я]* [А-Я A-Z][a-z а-я]*$");
+
+        }
+        static SHA256 sha256;
+        static Regex phoneRegex; //REGularEXpression
+        static Regex passwordRegex;
+        static Regex emailRegex;
+        static Regex nameRegex;
+
+
+        #region Name
+        private string name;
+        public string Name 
+        {   get => name;
+            set {
+                if (nameRegex.IsMatch(value))
+                {
+                    name = value;
+                }
+                else throw new InvalidDataException();
+            } 
+        }
+        #endregion
+
+        #region Phone
+        private string phone;
+		public string Phone
+		{
+            get => phone;
+			set => phone = value;
+		}
+        public void SetPhone(string _phone)
+        {
+            if (phoneRegex.IsMatch(_phone))
+            {
+                phone = "+7-(";
+                for (int i = 1; i < _phone.Length; i++)
+                {
+                    if (i == 4) phone += ")-";
+                    else if (i == 7 || i == 9) phone += "-";
+                    phone += _phone[i];
+                }
+            }
+            else throw new InvalidDataException();
+        }
+        #endregion
+
+        #region Email
+        private string email;
+		public string Email
+		{
+			get => email;
+			set 
+            {
+                if (emailRegex.IsMatch(value))
+                {
+                    email = value;
+                }
+                else throw new InvalidDataException();
+            }
+		}
+        #endregion
+
+        #region Login
+        private string login;
+		public string Login
+		{
+			get => login; 
+			set => login = value;
+		}
+        #endregion
+
+        #region Password
+        private string password;
+		public string Password
+		{
+            get => password;
+			set => password = value;
+		}
+        public void SetPassword(string password)
+        {
+                if (passwordRegex.IsMatch(password))
+                {
+                    var passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+                    var hash = Convert.ToHexString(sha256.ComputeHash(passwordBytes));
+                    password = hash;
+                }
+                else
+                {
+                    throw new InvalidDataException();
+                }
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nPhone: {Phone}\nEmail: {Email}\nLogin: {Login}\nPassword: {Password}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using CommonTask;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mail;
+
+string[] actions = new string[] // Список действий
+{
+    "---------------------------------------------------",
+    "a -- Посмотреть список пользователей",
+    "b -- Добавить пользователя",
+    "c -- Удалить пользователя",
+    "d -- Изменить пользователя",
+    "e -- Сохранить изменения в файл",
+    "f -- Отправить сообщение на e-mail пользователя",
+    "g -- Отсортировать по выбранному полю",
+    "q -- Выход",
+    "---------------------------------------------------"
+};
+string path = "persons.json";
+string name; // имя текущего пользователя для взаимодействия
+Person person; // текущий пользователь
+ConsoleKey inputKey; 
+var personsJson = File.ReadAllText(path);
+
+
+if (!File.Exists(path)) File.Create(path);
+List<Person> persons = JsonConvert.DeserializeObject<List<Person>>(personsJson) ?? new List<Person>();
+
+do
+{
+    
+    Console.WriteLine($"выберете действие:\n{string.Join('\n', actions)}");
+    inputKey = Console.ReadKey().Key;
+    
+    switch (inputKey)
+    {
+        // Посмотреть список пользователей
+        case ConsoleKey.A:
+            Console.Clear();
+            if (persons.Count == 0) Console.WriteLine("Вы не добавили не одного пользователя");
+            Console.WriteLine("----Пользователи----");
+            foreach (var _person in persons) Console.WriteLine(_person + "\n");
+            Console.WriteLine("--------------------");
+            break;
+        // Добавить пользователя
+        case ConsoleKey.B:
+            Console.Clear();
+            var newPerson = new Person();
+            try
+            {
+                Console.WriteLine("введите ФИО (ФИО должны быть с большой буквы и не содержать цифры): "); newPerson.Name = Console.ReadLine();
+                Console.WriteLine("введите номер телефона: "); newPerson.SetPhone(Console.ReadLine());
+                Console.WriteLine("введите почту: "); newPerson.Email = Console.ReadLine();
+                Console.WriteLine("введите логин: "); newPerson.Login = Console.ReadLine();
+                Console.WriteLine("введите пароль (пароль должен не менее 8 символов, содержать как минимум одну заглавную букву, одну строчную, одну цифру, один специальный символ): "); newPerson.SetPassword(Console.ReadLine());
+
+                persons.Add(newPerson);
+            }
+            catch (InvalidDataException e)
+            {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+            break;
+        // Удалить пользователя
+        case ConsoleKey.C:
+            Console.Clear();
+            PrintNames();
+            Console.WriteLine("введите полное имя пользователя, которого хотите удалить:");
+            name = Console.ReadLine();
+            if (persons.RemoveAll(person => person.Name.Contains(name)) > 0)
+                Console.WriteLine("Пользователь удален"); 
+            else Console.WriteLine("Пользователь не найден");
+            break;
+        case ConsoleKey.D:
+            Console.Clear();
+            PrintNames();
+            Console.WriteLine("введите полное имя пользователя, чьи данные хотите изменить");
+            name = Console.ReadLine();
+            person = persons.Find(person => person.Name.Contains(name));
+            try
+            {
+                Console.WriteLine("введите ФИО (ФИО должны быть с большой буквы и не содержать цифры): "); person.Name = Console.ReadLine();
+                Console.WriteLine("введите номер телефона: "); person.SetPhone(Console.ReadLine());
+                Console.WriteLine("введите почту: "); person.Email = Console.ReadLine();
+                Console.WriteLine("введите логин: "); person.Login = Console.ReadLine();
+                Console.WriteLine("введите пароль (пароль должен не менее 8 символов, содержать как минимум одну заглавную букву, одну строчную, одну цифру, один специальный символ): "); person.SetPassword(Console.ReadLine());
+                Console.WriteLine("----Данные успешно изменены----");
+            }
+            catch(InvalidDataException e)
+            {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+            break;
+        // Сохранить изменения в файл
+        case ConsoleKey.E:
+            Console.Clear();
+            SaveChanges(persons);
+            break;
+        // Отправить сообщение на e-mail пользователя
+        case ConsoleKey.F:
+            Console.Clear();
+            PrintNames();
+            Console.WriteLine("введите полное имя пользователя, на чью почту хотите отправить сообщение:");
+            name = Console.ReadLine();
+            person = persons.Find(person =>  person.Name.Contains(name));
+            Console.WriteLine("введите сообщение для отправки:");
+            var message = Console.ReadLine();
+            SendMessageToEmail(message, person.Email);
+            break;
+        // Отсортировать по выбранному полю
+        case ConsoleKey.G:
+            Console.Clear();
+            var properties = typeof(Person).GetProperties();
+            for (int i = 0; i < properties.Length; i++)
+                Console.WriteLine($"{i+1} -- {properties[i].Name}");
+            List<Person> sortedPersons = new();
+            Console.WriteLine("выберите поле для сортировки:");
+            if (int.TryParse(Console.ReadLine(), out int number) )
+            {
+                switch (number)
+                {
+                    case 0:
+                        sortedPersons = persons.OrderBy(person => person.Name).ToList();
+                        break;
+                    case 1:
+                        sortedPersons = persons.OrderBy(person => person.Phone).ToList();
+                        break;
+                    case 2:
+                        sortedPersons = persons.OrderBy(person  => person.Email).ToList();
+                        break;
+                    case 3:
+                        sortedPersons = persons.OrderBy(person => person.Login).ToList();
+                        break;
+                    case 4:
+                        sortedPersons = persons.OrderBy(person => person.Password).ToList();
+                        break;
+
+                }
+                Console.WriteLine($"----Отсортированные по {properties[number-1].Name} пользователи----");
+                sortedPersons.ForEach(_person => Console.WriteLine(_person + "\n"));
+                Console.WriteLine("------------------------------------");
+            }
+            else Console.WriteLine("ERROR: некорректно введен номер свойств");
+            break;
+    }
+
+} while (inputKey != ConsoleKey.Q);
+
+
+void PrintNames()
+{
+    var names = from person in persons select person.Name;
+    Console.WriteLine("----Доступные имена пользователей----");
+    foreach (var name in names)
+        Console.WriteLine(name);
+    Console.WriteLine("-------------------------------------");
+}
+void SaveChanges(List<Person> persons)
+{   
+    var serializePersons = JsonConvert.SerializeObject(persons, Formatting.Indented);
+    File.WriteAllText(path, serializePersons);
+    Console.WriteLine("Изменения сохранены в файл");
+}
+
+void SendMessageToEmail(string message, string recipient)
+{
+    using SmtpClient smtpClient = new SmtpClient("smtp.yandex.ru", 587);
+    smtpClient.EnableSsl = true;
+    smtpClient.Credentials = new NetworkCredential("bogdanevseyv@yandex.ru", "smdpvxfzbziwwpcb");
+    MailMessage mailMessage = new MailMessage();
+    mailMessage.From = new MailAddress("bogdanevseyv@yandex.ru");
+    mailMessage.To.Add(recipient);
+    mailMessage.Subject = "Spam from KubGU";
+    mailMessage.Body = message;
+
+
+    try
+    {
+        smtpClient.Send(mailMessage);
+        Console.WriteLine("Письмо успешно отправлено");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
